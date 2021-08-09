993d4b6a3210a6071c356449c4f0cca5928af448
aniket091 <75978424+aniket091@users.noreply.github.com>
2021-08-09 20:53:09 +0530

    ❔ Added Trivia

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports.reverseText = require('./functions/functions').reverseText;
 module.exports.Snake = require('./lib/Snake')
 module.exports.Connect4 = require('./lib/Connect4')
 module.exports.TicTacToe = require('./lib/Tic-Tac-Toe')
+module.exports.Trivia = require('./lib/Trivia')
 module.exports.RockPaperScissors = require('./lib/RockPaperScissors')
 module.exports.GuessThePokemon = require('./lib/GuessThePokemon');
 module.exports.EightBall = require('./functions/8ball')
