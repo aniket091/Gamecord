13df2a75db8666745c6618a251911c6621768656
Juhan280 <92646057+Juhan280@users.noreply.github.com>
2023-03-09 21:48:12 +0600

    add type declaration for FindEmoji

---
+++ types/FindEmoji.d.ts
@@ -0,0 +1,43 @@
+import {
+  ActionRowBuilder,
+  ButtonBuilder,
+  InteractionEditReplyOptions,
+  Message,
+  MessageEditOptions,
+  MessagePayload,
+} from 'discord.js';
+import { EventEmitter } from 'node:events';
+import { BaseConstructorOptions, ButtonStyle, DeepRequired, MessageType } from './Base';
+
+export interface FindEmojiConstructorOptions<IsSlashGame extends boolean> extends BaseConstructorOptions<IsSlashGame> {
+  embed?: {
+    title?: string;
+    color?: string;
+    description?: string;
+    findDescription?: string;
+  };
+  timeoutTime?: number;
+  hideEmojiTime?: number;
+  buttonStyle?: ButtonStyle;
+  emojis?: string[];
+  winMessage?: string;
+  loseMessage?: string;
+  timeoutMessage?: string;
+}
+
+export class FindEmoji<IsSlashGame extends boolean = false> extends EventEmitter {
+  options: DeepRequired<FindEmojiConstructorOptions<IsSlashGame>>;
+  message: MessageType<IsSlashGame>;
+  emojis: string[];
+  selected: string | null;
+  emoji: string | null;
+
+  constructor(options: FindEmojiConstructorOptions<IsSlashGame>);
+
+  sendMessage(
+    content: string | MessagePayload | (IsSlashGame extends true ? InteractionEditReplyOptions : MessageEditOptions)
+  ): Promise<Message>;
+  startGame(): Promise<void>;
+  gameOver(msg: Message, result: boolean): Promise<Message>;
+  getComponents(showEmoji: boolean): ActionRowBuilder<ButtonBuilder>;
+}
